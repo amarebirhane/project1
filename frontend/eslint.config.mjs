15b20a2e5710b4e0e346bf4a68a7dd53cb2b6474
amarebirhane <amarebirhane89@gmail.com>
2026-02-17 08:15:02 +0300

    updated the eslint

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -17,6 +17,13 @@ const eslintConfig = defineConfig([
     "coverage/**",
     "coverage/lcov-report",
   ]),
+  {
+    rules: {
+      "@typescript-eslint/no-explicit-any": "warn",
+      "@typescript-eslint/no-unused-vars": "warn",
+      "react/no-unescaped-entities": "off",
+    },
+  },
 ]);
 
 export default eslintConfig;
