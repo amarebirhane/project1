1061ae5231547405ab4428fecc5b45512bf5e197
amarebirhane <amarebirhane89@gmail.com>
2026-02-27 16:59:48 +0300

    added paginations

--- frontend/app/finance/list/ClientPage.tsx
+++ frontend/app/finance/list/ClientPage.tsx
@@ -968,6 +968,7 @@ export default function FinanceListPage() {
                         </td>
                       </tr>
                     ))}
+                  </TableBody>
                 </Table>
 
                 {filtered.length > 0 && (
